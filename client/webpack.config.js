0d3d1b7160d8d2286b94e9624a1f3dbe7bf453ab
kevin-coscarelli <kevinsl1991@gmail.com>
2023-04-10 18:53:37 +0200

    - Setup basico para la app de frontend

---
+++ client/webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.tsx',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'ts-loader',
+                },
+            },
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react'],
+                    },
+                },
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    devServer: {
+        static: ['dist'],
+        compress: true,
+        port: 9000,
+        hot: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+          template: './public/index.html',
+          filename: 'index.html',
+        }),
+      ],
+};
